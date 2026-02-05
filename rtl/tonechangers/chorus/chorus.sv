acd37a290208ddbde6b3a4dc4e148f72872e8f4d
kjconway5 <153889901+kjconway5@users.noreply.github.com>
2026-02-04 18:17:25 -0800

    file tree restructured

---
+++ rtl/tonechangers/chorus/chorus.sv
@@ -0,0 +1,13 @@
+module chorus #(
+  parameter int width = 16
+ ) (
+  input logic clk,
+  input logic rst,
+
+  input logic signed [width-1:0] in_signal,
+  output logic signed [width-1:0] out_signal
+
+);
+  
+
+endmodule
